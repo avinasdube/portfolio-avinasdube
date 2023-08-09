8d7904db3359dc374aee48a4e0984c747f100048
avinasdube <avidubey712@gamil.com>
2023-08-09 17:34:45 +0530

    [new]: added projects section

--- src/components/ProjectsSection/ProjectsSection.jsx
+++ src/components/ProjectsSection/ProjectsSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ProjectsSection.scss';
 
+import upright from '../../images/icons/upright.png';
 import mytask from '../../images/mytasks.jpg';
 import calculator from '../../images/calculator.jpg';
 import weatherin from '../../images/weatherin.jpg';
@@ -11,29 +12,32 @@ const ProjectsSection = () => {
         {
             id: 1,
             pImage: mytask,
-            pName: 'My Task - A Simple Task App',
+            pName: 'my task - a simple task app',
+            pDescrip: 'a functional task app with add, edit and delete task features. it uses localstorage feature to provide temporary data storage to remember previously added tasks.',
             pViewLink: 'https://avinasdube.github.io/responsive-todoapp/',
             codeViewLink: 'https://github.com/avinasdube/responsive-todoapp'
         },
         {
             id: 2,
             pImage: calculator,
-            pName: 'Simple Calculator App',
+            pName: 'simple calculator app',
+            pDescrip: 'a responsive calculator app with all basic functionalities',
             pViewLink: 'https://avinasdube.github.io/modern-calculator/',
             codeViewLink: 'https://github.com/avinasdube/modern-calculator'
         },
         {
             id: 3,
             pImage: weatherin,
-            pName: 'Weather.In - A Live Weather App',
+            pName: 'weather.in - a live weather app',
+            pDescrip: 'a live weather application built in react and nodejs which uses openweatherapp.org api to fetch live weather data. it also features light/dark mode button for better viewing experience.',
             pViewLink: 'https://avinasdube.github.io/weather.in/',
             codeViewLink: 'https://github.com/avinasdube/weather.in'
         },
     ];
-    
+
     return (
         <div className='projectsSection'>
-            <div className="projectsHead" id='prjcthd'>My Projects</div>
+            <div className="projectsHead" id='prjcthd'><span></span>my works <img src={upright} alt=''></img></div>
             <div className="projectsGrid" id='prjctgrd'>
                 {
                     projects.map(project => (
@@ -41,10 +45,13 @@ const ProjectsSection = () => {
                             <div className="projectImage" id='prjctimg'>
                                 <img src={project.pImage} alt=''></img>
                             </div>
-                            <div className="projectName" id='prjctnme'>{project.pName}</div>
-                            <div className="projectAction">
-                                <a href={project.pViewLink} id='vwprjctbtn'>View Live Project</a>
-                                <a href={project.codeViewLink} id='vwcdbtn'>View Code</a>
+                            <div className="projectAbout">
+                                <div className="projectName" id='prjctnme'>{project.pName}</div>
+                                <div className="projectDescrip" id='prjctdcrp'>{project.pDescrip}</div>
+                                <div className="projectActions">
+                                    <a href={project.pViewLink} id='vwbtn'>View Live Project</a>
+                                    <a href={project.codeViewLink} id='vwbtn'>View Code</a>
+                                </div>
                             </div>
                         </div>
                     ))
--- src/components/QuoteSection/QuoteSection.jsx
+++ src/components/QuoteSection/QuoteSection.jsx
@@ -4,7 +4,7 @@ import './QuoteSection.scss';
 const QuoteSection = () => {
     return (
         <div className='designQuote'>
-            <div className='qt' id='qyt'>"Design is the art of creating visual poetry. Let me do some for you !"</div>
+            <div className='qt' id='qyt'>"design is the art of creating visual poetry. let me do some for you !"</div>
         </div>
     )
 }
